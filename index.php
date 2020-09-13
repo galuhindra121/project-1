bd59f4169b4e7d67b0f99707e77f2cd002edf997
galuhindra121 <70931436+galuhindra121@users.noreply.github.com>
2020-09-13 13:37:15 +0700

    tuga ATW

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php
+
+CLASS Kucing{
+		public $nama, $jlh_kaki, $bisa_terbang, $suara;
+
+	}
+			$Kucing = new Kucing;
+			$Kucing->nama ="Momo";
+			$Kucing->jlh_kaki ="4";
+			$Kucing->bisa_terbang ="tidak";
+			$Kucing->suara ="Meong";
+
+echo "Kucing <br>";
+echo "Nama : $Kucing->nama";
+echo "<br>";
+echo "Jumlah Kaki : $Kucing->jlh_kaki";
+echo "<br>";
+echo "Bisa Terbang : $Kucing->bisa_terbang";
+echo "<br>";
+echo "Suara : $Kucing->suara";
+
+echo "<hr>";
+
+CLASS Anjing{
+		public $nama, $jlh_kaki, $bisa_terbang, $suara;
+
+	}
+			$Anjing = new Anjing;
+			$Anjing->nama ="Doggo";
+			$Anjing->jlh_kaki ="4";
+			$Anjing->bisa_terbang ="tidak";
+			$Anjing->suara ="Guk Guk";
+
+echo "Anjing <br>";
+echo "Nama : $Anjing->nama";
+echo "<br>";
+echo "Jumlah Kaki : $Anjing->jlh_kaki";
+echo "<br>";
+echo "Bisa Terbang : $Anjing->bisa_terbang";
+echo "<br>";
+echo "Suara : $Anjing->suara";
+
+echo "<hr>";
+
+CLASS Elang{
+		public $nama, $jlh_kaki, $bisa_terbang, $suara;
+
+	}
+
+			$Elang = new Elang;
+			$Elang->nama ="Zya";
+			$Elang->jlh_kaki ="2";
+			$Elang->bisa_terbang ="Bisa";
+			$Elang->suara ="miiiippp";
+
+echo "Elang <br>";
+echo "Nama : $Elang->nama";
+echo "<br>";
+echo "Jumlah Kaki : $Elang->jlh_kaki";
+echo "<br>";
+echo "Bisa Terbang : $Elang->bisa_terbang";
+echo "<br>";
+echo "Suara : $Elang->suara";
+
+echo "<hr>";
+
+	CLASS Angsa{
+		public $nama, $jlh_kaki, $bisa_terbang, $suara;
+
+	}
+			$Angsa = new Angsa;
+			$Angsa->nama ="Masha";
+			$Angsa->jlh_kaki ="2";
+			$Angsa->bisa_terbang ="tidak";
+			$Angsa->suara ="kwwaakk";
+
+echo "Angsa <br>";
+echo "Nama : $Angsa->nama";
+echo "<br>";
+echo "Jumlah Kaki : $Angsa->jlh_kaki";
+echo "<br>";
+echo "Bisa Terbang : $Angsa->bisa_terbang";
+echo "<br>";
+echo "Suara : $Angsa->suara";
+
+echo "<hr>";
+
+
+CLASS Ayam{
+		public $nama, $jlh_kaki, $bisa_terbang, $suara;
+
+	}
+
+			$Ayam = new Ayam;
+			$Ayam->nama ="Si Jago";
+			$Ayam->jlh_kaki ="2";
+			$Ayam->bisa_terbang ="tidak";
+			$Ayam->suara ="kukkuruyuk";
+
+echo "Ayam <br>";
+echo "Nama : $Ayam->nama";
+echo "<br>";
+echo "Jumlah Kaki : $Ayam->jlh_kaki";
+echo "<br>";
+echo "Bisa Terbang : $Ayam->bisa_terbang";
+echo "<br>";
+echo "Suara : $Ayam->suara";
+echo "<hr>";
